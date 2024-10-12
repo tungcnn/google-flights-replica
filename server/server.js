a7b7128bfe391af9de16f5c8a8b927e62e0289db
tungcnn <sontung.cnn@gmail.com>
2024-10-12 21:04:33 +1100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from the backend!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
